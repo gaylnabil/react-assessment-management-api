1364d8c69f5335a8fac978f8aa672933b8e9f478
Nabil GAYL <gaylnabil@gmail.com>
2023-04-04 18:25:31 +0000

    delete another unnecessary comments in SelectTag

--- frontend/src/components/inputs/SelectTag.jsx
+++ frontend/src/components/inputs/SelectTag.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 
 function SelectTag(props) {
 
-    // console.log('====================================');
-    // console.log("Select Tag: ", props.list.length !== 0);
-    // console.log('====================================');
     const optionsElement = props.list.map((obj) => (
         <option key={obj.id} value={obj.id} >{obj.name}</option>
     ));
