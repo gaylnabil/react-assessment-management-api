0566fbf4bbbc9a2a5e567540e3f2df37c5953dcd
Nabil GAYL <gaylnabil@gmail.com>
2023-04-07 18:14:33 +0000

    new docstring in javascript file index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+/**
+ * Creates a root React component and renders it to the DOM.
+ *
+ * @param {HTMLElement} element - The HTML element that will contain the rendered component.
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
