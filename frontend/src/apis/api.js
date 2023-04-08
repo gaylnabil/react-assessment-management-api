374f902955838ff263cf08f0f1d3ee14cd8d7717
NG Sakazaki <sakazaki.services@gmail.com>
2023-04-08 13:52:56 +0000

    new update in api js

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * Creates an instance of Axios with a base URL and headers set to JSON.
+ * @date 4/8/2023 - 1:51:14 PM
+ * @type {*}
+ */
 const api = axios.create({
   baseURL: `https://localhost:7146/api/`,
   headers: {
