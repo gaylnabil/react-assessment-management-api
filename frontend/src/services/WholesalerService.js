321c59f8943288bc87d703527807d9c4a814e94d
NG Sakazaki <sakazaki.services@gmail.com>
2023-04-08 15:23:36 +0000

    Update dostring in WholesalerService.js

--- frontend/src/services/WholesalerService.js
+++ frontend/src/services/WholesalerService.js
@@ -9,26 +9,32 @@ class WholesalerService {
     this.service = new ServiceProvider();
   }
 
+  // GET: url = wholesalers/${id}
   async getWholesaler(id) {
     return await this.service.get("wholesalers", id);
   }
 
+  // GET: url = wholesalers/
   async getAllWholesalers() {
     return await this.service.getAll("wholesalers");
   }
 
+  // POST: url = wholesalers/
   async addWholesaler(data) {
     return await this.service.post("wholesalers", data);
   }
 
+  // PUT: url = wholesalers/${id}
   async updateWholesaler(id, data) {
     return await this.service.update("wholesalers", id, data);
   }
 
+  // DELETE: url = wholesalers/${id}
   async deleteWholesaler(id) {
     return await this.service.delete("wholesalers", id);
   }
 
+  // GET: url = wholesalers/stocks/products
   async getWholesalersTheirStocks() {
     return await this.getWholesalersWith("wholesalers", "stocks", "products");
   }
