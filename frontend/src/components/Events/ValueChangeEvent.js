4defd83145e7ffbfb76acb67f86fc53f7ab1da8b
Nabil GAYL <gaylnabil@gmail.com>
2023-04-08 10:39:12 +0000

    update ValueChangeEvent js file

--- frontend/src/components/Events/ValueChangeEvent.js
+++ frontend/src/components/Events/ValueChangeEvent.js
@@ -1,3 +1,10 @@
+/**
+ * Updates form data based on input value changes.
+ *
+ * @param {Object} event - The event object triggered by the input change.
+ * @param {Function} setFormData - A function to update the form data state.
+ * @returns {Object} An updated version of the previous form data state with new values from changed inputs.
+ */
 const onValueChange = (event, setFormData) => {
   setFormData((prevData) => {
     // const {name, type, value, checked} = event.target;
