4cdbb28139782fa2b093da96b1cad4960d53376b
NG Sakazaki <sakazaki.services@gmail.com>
2023-04-10 12:11:51 +0000

    update Company.jsx file with new docstring

--- frontend/src/components/Company.jsx
+++ frontend/src/components/Company.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import Product from './products/Product'
 
+/**
+ * Renders a Company component with its name and list of products.
+ *
+ * @param {Object} props - The properties object containing the following:
+ *   @param {string} props.name - The name of the company to display.
+ *   @param {Array<Object>} props.products - An array of product objects to display.
+ *   @param {function} props.deleteProduct - A function to delete a product from the list.
+ *   @param {function} props.onSelectProduct - A function to select a product from the list.
+ *
+ * @returns {JSX.Element} Returns JSX element that displays company information and products. 
+ */
 function Company(props) {
     const productElements = props.products.map((product) => (
 
